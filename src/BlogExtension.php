bab01294360bc2ecbb8d7e09fab7dee2193b515f
Rixafy <rixafy@gmail.com>
2019-03-23 17:50:36 +0100

    Move ORM path register to BlogExtension class

--- src/BlogExtension.php
+++ src/BlogExtension.php
@@ -6,5 +6,9 @@ namespace Archette\Blog;
 
 class BlogExtension extends \Nette\DI\CompilerExtension
 {
-
+    public function beforeCompile()
+    {
+        $this->getContainerBuilder()->getDefinitionByType(\Doctrine\Common\Persistence\Mapping\Driver\AnnotationDriver::class)
+            ->addSetup('addPaths', [['vendor/rixafy/blog']]);
+    }
 }
